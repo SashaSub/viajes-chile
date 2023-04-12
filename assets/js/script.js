f29ebc7fb3ca4855e4fd7914896200708c6c811c
SashaSub <ssuboch@gmail.com>
2023-04-12 10:16:26 -0400

    Toggle el texto de la sección Quienes Somos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,23 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
+// alert mensaje con el boton Enviar
 $(document).ready(function(){
     $("#enviarMensaje").click(function(){
         alert("El mensaje fue enviado!");
     });
 });
 
+// cambio de color a rojo de los links de la barra de navegación con click
 $(document).ready(function(){
     $(".nav-link").on("click", function(){
         $(this).css({
             "color": "red"
         });
     });
+});
+
+// toggle el texto de la seccion QUIENES SOMOS
+$("#quienes-somos h2").click(function(){
+    $("p").toggle();
 });
